259cc5b636ba9000e1c7b0917c2227bb7c901cfd
Mark Bennett <mark@burmis.ca>
2022-06-10 20:37:04 +0000

    Specify another test

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,5 @@
 describe("processEvent", () => {
   it.todo("should convert the current_url to lowercase");
   it.todo("should remove the trailing slash from the current_url");
+  it.todo("should preserve trailing id anchors");
 });
