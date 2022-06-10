065db8c4dbe80490e3855b2461deb475625ed933
Mark Bennett <mark@burmis.ca>
2022-06-10 18:24:29 +0000

    Add a basic test with placeholders.

---
+++ index.test.ts
@@ -0,0 +1,7 @@
+describe("processEvent", () => {
+  it.todo("should convert the current_url to lowercase");
+  it.todo("should remove the trailing slash from the current_url");
+  it.todo(
+    "should store the normalized value in the property set in the config"
+  );
+});
