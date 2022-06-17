4bf687d72a41da8521601ad3fb5f922eeda6e121
Mark Bennett <mark@burmis.ca>
2022-06-17 20:31:36 +0000

    First test pass.

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,43 @@
+import { PluginEvent, PluginInput, PluginMeta } from "@posthog/plugin-scaffold";
+import { processEvent } from "./index";
+
+/**
+ * Given a url, construct a page view event.
+ *
+ * @param current_url The current url of the page view
+ * @returns A new PostHog page view event
+ */
+function buildPageViewEvent(current_url: string): PluginEvent {
+  const event: PluginEvent = {
+    properties: { current_url },
+    distinct_id: "distinct_id",
+    ip: "1.2.3.4",
+    site_url: "test.com",
+    team_id: 0,
+    now: "2022-06-17T20:21:31.778000+00:00",
+    event: "$pageview",
+    uuid: "01817354-06bb-0000-d31c-2c4eed374100",
+  };
+
+  return event;
+}
+
+function getMeta(): PluginMeta<PluginInput> {
+  return {} as unknown as PluginMeta<PluginInput>;
+}
+
 describe("processEvent", () => {
+  it("shouldn't change a url that's already lowercase", async () => {
+    const sourceEvent = buildPageViewEvent("http://www.google.com");
+
+    const processedEvent = await processEvent(sourceEvent, getMeta());
+
+    console.log("processedEvent = ", processedEvent);
+
+    expect(processedEvent?.properties?.current_url).toEqual(
+      "http://www.google.com"
+    );
+  });
   it.todo("should convert the current_url to lowercase");
   it.todo("should remove the trailing slash from the current_url");
   it.todo("should preserve trailing id anchors");
--- index.ts
+++ index.ts
@@ -1,22 +1,11 @@
-// <TODO: your plugin code here - you can base it on the code below, but you don't have to>
+import { PluginEvent, PluginInput, PluginMeta } from "@posthog/plugin-scaffold";
 
-// Some internal library function
-async function getRandomNumber() {
-    return 4
-}
-
-// Plugin method that runs on plugin load
-export async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin`)
-}
-
-// Plugin method that processes event
-export async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
-    return event
+export function processEvent(
+  event: PluginEvent,
+  meta: PluginMeta<PluginInput>
+) {
+  if (event.properties) {
+    event.properties["hello"] = "world";
+  }
+  return event;
 }
